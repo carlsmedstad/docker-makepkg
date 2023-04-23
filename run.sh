9a6818cf6e03c50f9318adfb6698384a319f49c8
Carl Smedstad <carl.smedstad@protonmail.com>
2023-04-23 09:22:44 +0200

    Format run.sh with shfmt

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 set -e
 
 # Make a copy so we never alter the original
-cp -r /pkg /tmp/pkg
+cp -r /pkg /tmp/pkg || :
 cd /tmp/pkg
 rm -rf pkg ./*.tar.zst
 
 # Sync database
 if [ -n "$SYNC_DATABASE" ]; then
-    paru -S --refresh
+  paru -S --refresh
 fi
 
 # Install checkdepends
@@ -24,12 +24,12 @@ paru -U --noconfirm
 
 # Store the built package(s). Ensure permissions match the original PKGBUILD.
 if [ -n "$EXPORT_PKG" ]; then
-    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./*pkg.tar*
-    sudo mv ./*pkg.tar* /pkg
+  sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./*pkg.tar*
+  sudo mv ./*pkg.tar* /pkg
 fi
 # Export .SRCINFO for built package
 if [ -n "$EXPORT_SRC" ]; then
-    makepkg --printsrcinfo > .SRCINFO
-    sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./.SRCINFO
-    sudo mv ./.SRCINFO /pkg
+  makepkg --printsrcinfo > .SRCINFO
+  sudo chown "$(stat -c '%u:%g' /pkg/PKGBUILD)" ./.SRCINFO
+  sudo mv ./.SRCINFO /pkg
 fi
